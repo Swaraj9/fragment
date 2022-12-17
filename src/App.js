f43f706b54fb95d7a3e92de3a06a45fdf38f037e
Swaraj9 <swaraj.dusane90@gmail.com>
2022-12-18 00:37:08 +0530

    SVGs

--- src/App.js
+++ src/App.js
@@ -12,6 +12,45 @@ import { motion, useScroll } from 'framer-motion';
 
 const backgroundColors = ['rgb(0,0,0,0.3)', 'rgb(102, 35, 106, 0.3)', 'rgb(40, 153, 203, 0.3)', 'rgb(80, 13, 239, 0.3)', 'rgb(216, 19, 115, 0.3)'];
 
+const Floater = ({speed, children}) => {
+  
+  const [x, setX] = useState(Math.random()*500);
+  const [speedX, setSpeedX] = useState(speed);
+  const [y, setY] = useState(Math.random()*500);
+  const [speedY, setSpeedY] = useState(speed);
+
+  return(
+    <motion.div
+      animate={{ x: x, y: y}}
+      transition={{
+        ease: "linear",
+        //duration: 0.001
+        // repeat: Infinity
+      }}
+      onAnimationComplete={() => {
+        setX(x + speedX);
+        setY(y + speedY);
+        if (x <= 100) {
+          setSpeedX(speed);
+        }
+        if (x >= window.innerWidth - 500) {
+          setSpeedX(-speed);
+        }
+
+        if (y <= 100) {
+          setSpeedY(speed);
+        }
+
+        if (y >= window.innerHeight - 500) {
+          setSpeedY(-speed);
+        }
+      }}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
 function App() {
   const [vantaEffect, setVantaEffect] = useState(null)
   const myRef = useRef(null)
@@ -22,6 +61,7 @@ function App() {
   const {scrollYProgress} = useScroll({container: scrollDiv})
 
   const scaleY = scrollYProgress
+
 
   useEffect(() => {
     if (!vantaEffect) {
@@ -57,10 +97,42 @@ function App() {
   return (
     <div className="App" ref={myRef} style={{width:'100%', color:'rgb(235,235,235)', height:'100vh', overflow:'hidden'}} >
       <div ref={scrollDiv} onScroll={handleScroll} className='main' style={{width:'100%', display:'flex', transitionProperty: 'all', transitionDuration:'1s', scrollBehavior:'smooth', backgroundColor:backgroundColors[currBackgroundColor], height:'100%', overflowY:'scroll', scrollbarWidth:'none'}}>
+        <div style={{position:'fixed', zIndex:-10}}>
+          <Floater speed={10}>
+            <motion.div
+              animate={{rotate:[0,360]}}
+              transition={{repeat:'Infinity', duration:7, ease:'linear', delay:0}}
+            >
+              <svg height="210" width="500">
+                <polygon points="200,10 250,190 160,210" style={{fill:'rgb(185,185,185,0.2)','stroke':'rgb(195,195,195,0.3)','stroke-width':2}} />
+              </svg>
+            </motion.div>
+          </Floater>
+          <Floater speed={30}>
+            <motion.div
+              animate={{rotate:[0,360]}}
+              transition={{repeat:'Infinity', duration:7, ease:'linear', delay:0}}
+            >
+              <svg height="210" width="500">
+                <polygon points="200,10 250,190 160,210 80,170" style={{fill:'rgb(185,185,185,0.2)','stroke':'rgb(195,195,195,0.3)','stroke-width':2}} />
+              </svg>
+            </motion.div>
+          </Floater>
+          <Floater speed={50}>
+            <motion.div
+              animate={{rotate:[0,360]}}
+              transition={{repeat:'Infinity', duration:7, ease:'linear', delay:0}}
+            >
+              <svg height="210" width="500">
+                <polygon points="250,190 160,210 80,130 20,80" style={{fill:'rgb(185,185,185,0.2)','stroke':'rgb(195,195,195,0.3)','stroke-width':2}} />
+              </svg>
+            </motion.div>
+          </Floater>
+        </div>
         <div style={{flex:'1'}}>
           <Navbar/>
           <Home/>
-          <Services scrollRef={scrollDiv}/>
+          <Services/>
           <Portfolio/>
           <About/>
           <Footer/>
