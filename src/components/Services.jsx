89574a120432c872ae150927e7d24160408f8b76
Swaraj9 <swaraj.dusane90@gmail.com>
2022-12-29 12:33:07 +0530

    Wire 2

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -27,7 +27,7 @@ const Subcard = ({title, text, index})=>{
     <motion.div
       initial={{opacity:0, translateY: 200}}
       whileInView={{opacity:1, translateY: 0}}
-      transition={{duration:0.3, delay: window.innerWidth >= 600 ? index*0.2 : 0}}
+      transition={{duration:0.7, delay: window.innerWidth >= 600 ? index*0.2 : 0}}
       viewport={{margin:'400px'}}
       ref = {myRef}
       className='subCard'
