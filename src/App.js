a1b35b40c87369b058aaf6ccf3142b53851fffe7
Swaraj9 <swaraj.dusane90@gmail.com>
2023-01-30 23:47:25 +0530

    F

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import {useState, useRef, useEffect} from 'react'
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Services from './components/Services';
-import Footer from './components/Footer';
-import './styles/app.css'
-import { motion, useScroll} from 'framer-motion';
-import loader from './images/fragment.gif';
-import { CursorContextProvider } from './cursorContext';
-import TOPOLOGY from 'vanta/dist/vanta.topology.min'
-import p5 from 'p5'
-import ScrollButton from './components/ScrollButton';
-import CaseStudy from './components/CaseStudy';
-
+import { useState, useRef, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import "./styles/app.css";
+import { motion, useScroll } from "framer-motion";
+import loader from "./images/fragment.gif";
+import { CursorContextProvider } from "./cursorContext";
+import TOPOLOGY from "vanta/dist/vanta.topology.min";
+import p5 from "p5";
+import ScrollButton from "./components/ScrollButton";
+import CaseStudy from "./components/CaseStudy";
 
 function App() {
-
   const [loading, setLoading] = useState(true);
 
   const [inHome, setInHome] = useState(false);
@@ -22,70 +20,119 @@ function App() {
   const [caseStudy, setCaseStudy] = useState(false);
 
   const scrollDiv = useRef(null);
-  const {scrollYProgress} = useScroll({container: scrollDiv})
+  const { scrollYProgress } = useScroll({ container: scrollDiv });
 
-  const scaleY = scrollYProgress
+  const scaleY = scrollYProgress;
 
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const vantaRef = useRef(null)
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
 
   useEffect(() => {
     if (!vantaEffect) {
-      setVantaEffect(TOPOLOGY({
-        el: vantaRef.current,
-        p5: p5,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.0,
-        color: 0x8a2be2,
-        backgroundColor: 0x0,
-      }))
+      setVantaEffect(
+        TOPOLOGY({
+          el: vantaRef.current,
+          p5: p5,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          color: 0x8a2be2,
+          backgroundColor: 0x0,
+        })
+      );
     }
     return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect, loading]);
 
   useEffect(() => {
     setLoading(true);
-    setTimeout(()=>{
+    setTimeout(() => {
       setLoading(false);
-    }, 2500)
-  }, [])
+    }, 2500);
+  }, []);
 
   return (
     <CursorContextProvider>
-      <div style={{width:'100vw', height:'100vh', overflow:'hidden'}}>
-        {loading ? <div style={{backgroundColor:'black', display:'flex', alignItems:'center', justifyContent:'center', width: '100%', height:'100%'}}><img width='80%' style={{maxWidth:'800px'}} src={loader} alt="Loader GIF"/></div>
-        : <div 
-          className="App" 
-          style={{width:'100%', color:'rgb(235,235,235)', height:'100%', overflow:'hidden'}} 
-          ref={vantaRef}
-        >
-            <div 
-              ref={scrollDiv} 
-              className='main' 
-              style={{display:loading?'none':'flex',width:'100%', transitionProperty: 'all', transitionDuration:'1s', scrollBehavior:'smooth', height:'100%', overflowY:'scroll', scrollbarWidth:'none', backgroundColor:'rgb(0,0,0,0.5)'}}
+      <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+        {loading && 
+          <div
+            style={{
+              backgroundColor: "black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <img
+              width="80%"
+              style={{ maxWidth: "800px" }}
+              src={loader}
+              alt="Loader GIF"
+            />
+          </div>
+        }
+        
+          <div
+            className="App"
+            style={{
+              width: "100%",
+              color: "rgb(235,235,235)",
+              height: "100%",
+              overflow: "hidden",
+            }}
+            ref={vantaRef}
+          >
+            <div
+              ref={scrollDiv}
+              className="main"
+              style={{
+                display: loading ? "none" : "flex",
+                width: "100%",
+                transitionProperty: "all",
+                transitionDuration: "1s",
+                scrollBehavior: "smooth",
+                height: "100%",
+                overflowY: "scroll",
+                scrollbarWidth: "none",
+                backgroundColor: "rgb(0,0,0,0.5)",
+              }}
             >
-              <motion.div 
-                style={{flex:'1'}}              
-                initial={{opacity:0, backgroundColor:'black'}} 
-                animate={{opacity:1, backgroundColor:'transparent'}} 
-                transition={{duration:4}} 
+              <motion.div
+                style={{ flex: "1" }}
+                initial={{ opacity: 0, backgroundColor: "black" }}
+                animate={{ opacity: 1, backgroundColor: "transparent" }}
+                transition={{ duration: 4 }}
               >
-                  <Navbar caseStudy={caseStudy} setCaseStudy={setCaseStudy}/>
-                  {!caseStudy ?
-                    <div style={{display:'flex', flexDirection:'column'}}>
-                      <Home inHome={inHome} setInHome={setInHome}/>
-                      <Services/>
-                      <Footer setCaseStudy={setCaseStudy}/>
-                  </div> : <CaseStudy/>}
+                <Navbar caseStudy={caseStudy} setCaseStudy={setCaseStudy} />
+                {!caseStudy ? (
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <Home inHome={inHome} setInHome={setInHome} />
+                    <Services />
+                    <Footer setCaseStudy={setCaseStudy} />
+                  </div>
+                ) : (
+                  <CaseStudy />
+                )}
               </motion.div>
-              <motion.div className='scrollBar' style={{scaleY, position:"sticky", top:0, width:'0.25rem', backgroundColor:'rgb(235,235,235)'}}></motion.div>
-              <ScrollButton inHome={inHome}/>    
+              <motion.div
+                className="scrollBar"
+                style={{
+                  scaleY,
+                  position: "sticky",
+                  top: 0,
+                  width: "0.25rem",
+                  backgroundColor: "rgb(235,235,235)",
+                }}
+              ></motion.div>
+              <ScrollButton inHome={inHome} />
             </div>
-          </div>}
+          </div>
+        
       </div>
     </CursorContextProvider>
   );
