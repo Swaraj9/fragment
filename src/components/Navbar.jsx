d5201bdd8f1d80e2a46ddcc45def173f1e17854b
Swaraj9 <swaraj.dusane90@gmail.com>
2023-05-13 15:36:31 +0530

    Changed Favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Button from './Button'
 import '../styles/navbar.css'
 import '../styles/button.css'
+import logo from '../images/Iconbg.png'
 
 const Navbar = ({caseStudy, setCaseStudy}) => {
   return (
     <div id="navbar" className='navbar'>
-      <div className='navbarTitle'>Fragment</div>
+      <div style={{display:"flex", alignItems:'center'}}>
+        <img alt="logo" src={logo} height="50px" style={{marginRight:'1rem'}}/>
+        <div className='navbarTitle'>Fragment</div>
+      </div>
       {!caseStudy ? <a href='#footer'><Button className='Button navbarButton'>Get in Touch</Button></a>:
         <Button onClick={() => setCaseStudy(false)} className='Button navbarButton'>Home</Button>
       }
